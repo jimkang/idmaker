d1a284a15cd21338c7e4e521cf0c9ca6aac8fbc6
Jim Kang <jimkang@gmail.com>
2019-11-24 13:30:49 -0500

    Support upper mode in command line util.

--- randomid.js
+++ randomid.js
@@ -5,12 +5,21 @@
 var RandomId = require('./index');
 
 if (process.argv.length < 3) {
-  console.log('Usage: [node] randomid <length of id>');
+  console.log(
+    'Usage: [node] randomid <length of id> [--upper, provide this to generate ids with only uppercase alpha chars]'
+  );
   process.exit();
 }
 
-var length = +process.argv[2];
+const length = +process.argv[2];
 
-var randomId = RandomId();
+var opts = {};
+if (process.argv.length > 3) {
+  if (process.argv[3] === '--upper') {
+    opts.idChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  }
+}
+
+var randomId = RandomId(opts);
 
 console.log(randomId(length));
